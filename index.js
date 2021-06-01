3f31625f9cf71395a81803f02934906626b2a2f0
Emmanuel Rdz P <emmanuel711721mail.com>
2021-06-01 09:01:12 -0600

    Startup variables

--- index.js
+++ index.js
@@ -1,7 +1,61 @@
 //Modulos importados
 let express = require('express');
-let app = express();
-let http = require('http').createServer(app);
+let app 	= express();
+let http 	= require('http').createServer(app);
+let io 		= require('socket.io')(http);
+let mysql 	=require('mysql');
+let datos = {
+	CO2: '',
+	CH4: '',
+	O2:  '',
+	H2S: '',
+	HR: '',
+	time_conc: '',
+	ID_conc: '',
+
+	Patm: '',
+	Pint: '',
+	Pent: '',
+	time_pres: '',
+	ID_pres: '',
+
+	Tint: '',
+	Tgas: '',
+	Tatm: '',
+	time_temp: '',
+	ID_temp: '',
+
+	V1: '',
+	V2: '',
+	V3: '',
+	V4: '',
+	V5: '',
+	V6: '',
+	Vinicio: '',
+	time_valv: '',
+	ID_valv: '',
+
+	CO2_e: '',
+	CH4_e: '',
+	O2_e:  '',
+	H2S_e: '',
+	HR_e: '',
+	Patm_e: '',
+	Pint_e: '',
+	Pent_e: '',
+	Tint_e: '',
+	Tgas_e: '',
+	Tatm_e: '',
+	V1_e: '',
+	V2_e: '',
+	V3_e: '',
+	V4_e: '',
+	V5_e: '',
+	V6_e: '',
+	Vinicio_e: '',
+	time_edos: '',
+	ID_edos: ''
+};
 
 //************* Alta del Server*****************//
 app.get('/', (req, res)=>{
@@ -13,3 +67,99 @@ app.use('/src', express.static('./src'));
 http.listen(3000, ()=>{
 	console.log('Server Monitoreo-web-reactor: Activo en 3000')
 });
+
+//********** MYSQL
+let dataBase = mysql.createConnection({
+	host: 'localhost',
+	user: 'admin',
+	password: '1234'
+});
+
+//********** SOCKET.IO
+io.on('connection', (socket)=>{
+	socket.on('startup',(data)=>{
+		dataBase.query("USE reactor40", function (err, result,field) {
+			if (err) throw err;
+		});
+		dataBase.query('SELECT * FROM concentracion ORDER BY ID DESC;',function(err, result, field){
+			if (err) throw err;
+			datos.CO2 = result[0].CO2;
+			datos.CH4 = result[0].CH4;
+			datos.O2 = result[0].O2;
+			datos.H2S = result[0].H2S;
+			datos.HR = result[0].HR;
+			datos.time_conc = result[0].time;
+			datos.ID_conc = result[0].ID;
+			pedir_presiones();
+		});
+		
+			function pedir_presiones(){
+				dataBase.query('SELECT * FROM presiones ORDER BY ID DESC;',function(err, result, field){
+					if (err) throw err;
+					datos.Patm = result[0].Patm;
+					datos.Pint = result[0].Pint;
+					datos.Pent = result[0].Pent;
+					datos.time_pres = result[0].time;
+					datos.ID_pres = result[0].ID;
+					pedir_temperaturas();
+				});
+			}
+			function pedir_temperaturas(){
+				dataBase.query('SELECT * FROM temperaturas ORDER BY ID DESC;',function(err, result, field){
+					if (err) throw err;
+					datos.Tint = result[0].Tint;
+					datos.Tgas = result[0].Tgas;
+					datos.Tatm = result[0].Tatm;
+					datos.time_temp = result[0].time;
+					datos.ID_temp = result[0].ID;
+					pedir_valvulas();
+				});
+			}
+			function pedir_valvulas(){
+				dataBase.query('SELECT * FROM valvulas ORDER BY ID DESC;',function(err, result, field){
+					if (err) throw err;
+					datos.V1 = result[0].V1;
+					datos.V2 = result[0].V2;
+					datos.V3 = result[0].V3;
+					datos.V4 = result[0].V4;
+					datos.V5 = result[0].V5;
+					datos.V6 = result[0].V6;
+					datos.Vinicio = result[0].Vinicio;
+					datos.time_valv = result[0].time;
+					datos.ID_valv = result[0].ID;
+					pedir_estados();
+				});
+			}
+			function pedir_estados(){
+				dataBase.query('SELECT * FROM estados ORDER BY ID DESC;',function(err, result, field){
+					if (err) throw err;
+					datos.CO2_e = result[0].CO2;
+					datos.CH4_e = result[0].CH4;
+					datos.O2_e = result[0].O2;
+					datos.H2S_e = result[0].H2S;
+					datos.HR_e = result[0].HR;
+					datos.Patm_e = result[0].Patm;
+					datos.Pint_e = result[0].Pint;
+					datos.Pent_e = result[0].Pent;
+					datos.Tint_e = result[0].Tint;
+					datos.Tgas_e = result[0].Tgas;
+					datos.Tatm_e = result[0].Tatm;
+					datos.V1_e = result[0].V1;
+					datos.V2_e = result[0].V2;
+					datos.V3_e = result[0].V3;
+					datos.V4_e = result[0].V4;
+					datos.V5_e = result[0].V5;
+					datos.V6_e = result[0].V6;
+					datos.Vinicio_e = result[0].Vinicio;
+					datos.time_edos = result[0].time;
+					datos.ID_edos = result[0].ID;
+					let datjason = JSON.stringify(datos);
+					io.to(socket.id).emit('startup',datjason);
+				});
+			}
+
+	});
+});
+//console.log(JSON.stringify(concentracion));
+			//let Msg= 'Bienvenido El dato CO2 es: ' + concentracion[0];
+			//io.to(socket.id).emit('startup',Msg);
--- src/codigo.js
+++ src/codigo.js
@@ -1,3 +1,36 @@
+
+
+const socket = io();
+let CO2DOM = document.getElementById('CO2');
+let CH4DOM = document.getElementById('CH4');
+let O2DOM = document.getElementById('O2');
+let H2SDOM = document.getElementById('H2S');
+let HRDOM = document.getElementById('HR');
+let PatmDOM = document.getElementById('Patm');
+let PintDOM = document.getElementById('Pint');
+let PentDOM = document.getElementById('Pent');
+let TintDOM = document.getElementById('Tint');
+let TgasDOM = document.getElementById('Tgas');
+let TatmDOM = document.getElementById('Tatm');
+let CO2_eDOM = document.getElementById('CO2_e');
+let CH4_eDOM = document.getElementById('CH4_e');
+let O2_eDOM = document.getElementById('O2_e');
+let H2S_eDOM = document.getElementById('H2S_e');
+let HR_eDOM = document.getElementById('HR_e');
+let Patm_eDOM = document.getElementById('Patm_e');
+let Pint_eDOM = document.getElementById('Pint_e');
+let Pent_eDOM = document.getElementById('Pent_e');
+let Tint_eDOM = document.getElementById('Tint_e');
+let Tgas_eDOM = document.getElementById('Tgas_e');
+let Tatm_eDOM = document.getElementById('Tatm_e');
+let datosDOM;
+let CO2_cliente;//Ejemplo
+//id a todas las variables en HTML
+//Obtener con el DOM el valor
+//Crear un objeto global para los valores leidos
+//funcion de actualizar valores leidos
+//Tarea para Lunes 31/mayo
+
 //********************Prueba de Grafica
 const CHART = document.getElementById('LineChart');
 console.log(CHART);
@@ -430,3 +463,288 @@ btn6DOM.addEventListener('click', function(){
 	document.getElementById('div6').style.display = '';
 	document.getElementById('out').style.display = 'none';
 });
+
+
+
+socket.emit('startup', 1);
+
+socket.on('startup', function(data){
+	datosDOM = JSON.parse(data);
+	actualizar_valores();
+	actualizar_valvulashw();
+	//console.log(data2.HR);
+});
+
+function actualizar_valores(){
+	CO2DOM.innerHTML = JSON.parse(datosDOM.CO2);
+	CH4DOM.innerHTML = JSON.parse(datosDOM.CH4);
+	O2DOM.innerHTML = JSON.parse(datosDOM.O2);
+	H2SDOM.innerHTML = JSON.parse(datosDOM.H2S);
+	HRDOM.innerHTML = JSON.parse(datosDOM.HR);
+	PatmDOM.innerHTML = JSON.parse(datosDOM.Patm);
+	PintDOM.innerHTML = JSON.parse(datosDOM.Pint);
+	PentDOM.innerHTML = JSON.parse(datosDOM.Pent);
+	TintDOM.innerHTML = JSON.parse(datosDOM.Tint);
+	TgasDOM.innerHTML = JSON.parse(datosDOM.Tgas);
+	TatmDOM.innerHTML = JSON.parse(datosDOM.Tatm);
+	//****************************
+	if (datosDOM.CO2_e== 'X') {
+		CO2_e.innerHTML = 'OFF';
+		document.getElementById('abc').classList.add("OFF");
+		document.getElementById('CO2_est').classList.add("RED");
+	} else if (datosDOM.CO2_e== 'A') {
+		CO2_e.innerHTML = 'ON';
+		document.getElementById('abc').classList.add("ON");
+		document.getElementById('CO2_est').classList.add("GREEN");
+	} else if (datosDOM.CO2_e== 'O') {
+		CO2_e.innerHTML = 'OFF LINE';
+		document.getElementById('abc').classList.add("OFF-line");
+		document.getElementById('CO2_est').classList.add("GRAY");
+	}else if (datosDOM.CO2_e== 'M') {
+		CO2_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('abc').classList.add("mto");
+		document.getElementById('CO2_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.CH4_e== 'X') {
+		CH4_e.innerHTML = 'OFF';
+		document.getElementById('edo_CH4').classList.add("OFF");
+		document.getElementById('CH4_est').classList.add("RED");
+	} else if (datosDOM.CH4_e== 'A') {
+		CH4_e.innerHTML = 'ON';
+		document.getElementById('edo_CH4').classList.add("ON");
+		document.getElementById('CH4_est').classList.add("GREEN");
+	} else if (datosDOM.CH4_e== 'O') {
+		CH4_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_CH4').classList.add("OFF-line");
+		document.getElementById('CH4_est').classList.add("GRAY");
+	}else if (datosDOM.CH4_e== 'M') {
+		CH4_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_CH4').classList.add("mto");
+		document.getElementById('CH4_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.O2_e== 'X') {
+		O2_e.innerHTML = 'OFF';
+		document.getElementById('edo_O2').classList.add("OFF");
+		document.getElementById('O2_est').classList.add("RED");
+	} else if (datosDOM.O2_e== 'A') {
+		O2_e.innerHTML = 'ON';
+		document.getElementById('edo_O2').classList.add("ON");
+		document.getElementById('O2_est').classList.add("GREEN");
+	} else if (datosDOM.O2_e== 'O') {
+		O2_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_O2').classList.add("OFF-line");
+		document.getElementById('O2_est').classList.add("GRAY");
+	}else if (datosDOM.O2_e== 'M') {
+		O2_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_O2').classList.add("mto");
+		document.getElementById('O2_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.H2S_e== 'X') {
+		H2S_e.innerHTML = 'OFF';
+		document.getElementById('edo_H2S').classList.add("OFF");
+		document.getElementById('H2S_est').classList.add("RED");
+	} else if (datosDOM.H2S_e== 'A') {
+		H2S_e.innerHTML = 'ON';
+		document.getElementById('edo_H2S').classList.add("ON");
+		document.getElementById('H2S_est').classList.add("GREEN");
+	} else if (datosDOM.H2S_e== 'O') {
+		H2S_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_H2S').classList.add("OFF-line");
+		document.getElementById('H2S_est').classList.add("GRAY");
+	}else if (datosDOM.H2S_e== 'M') {
+		H2S_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_H2S').classList.add("mto");
+		document.getElementById('H2S_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.HR_e== 'X') {
+		HR_e.innerHTML = 'OFF';
+		document.getElementById('edo_HR').classList.add("OFF");
+		document.getElementById('HR_est').classList.add("RED");
+	} else if (datosDOM.HR_e== 'A') {
+		HR_e.innerHTML = 'ON';
+		document.getElementById('edo_HR').classList.add("ON");
+		document.getElementById('HR_est').classList.add("GREEN");
+	} else if (datosDOM.HR_e== 'O') {
+		HR_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_HR').classList.add("OFF-line");
+		document.getElementById('HR_est').classList.add("GRAY");
+	}else if (datosDOM.HR_e== 'M') {
+		HR_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_HR').classList.add("mto");
+		document.getElementById('HR_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.Patm_e== 'X') {
+		Patm_e.innerHTML = 'OFF';
+		document.getElementById('edo_Patm').classList.add("OFF");
+		document.getElementById('Patm_est').classList.add("RED");
+	} else if (datosDOM.Patm_e== 'A') {
+		Patm_e.innerHTML = 'ON';
+		document.getElementById('edo_Patm').classList.add("ON");
+		document.getElementById('Patm_est').classList.add("GREEN");
+	} else if (datosDOM.Patm_e== 'O') {
+		Patm_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Patm').classList.add("OFF-line");
+		document.getElementById('Patm_est').classList.add("GRAY");
+	}else if (datosDOM.Patm_e== 'M') {
+		Patm_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Patm').classList.add("mto");
+		document.getElementById('Patm_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.Pint_e== 'X') {
+		Pint_e.innerHTML = 'OFF';
+		document.getElementById('edo_Pint').classList.add("OFF");
+		document.getElementById('Pint_est').classList.add("RED");
+	} else if (datosDOM.Pint_e== 'A') {
+		Pint_e.innerHTML = 'ON';
+		document.getElementById('edo_Pint').classList.add("ON");
+		document.getElementById('Pint_est').classList.add("GREEN");
+	} else if (datosDOM.Pint_e== 'O') {
+		Pint_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Pint').classList.add("OFF-line");
+		document.getElementById('Pint_est').classList.add("GRAY");
+	}else if (datosDOM.Pint_e== 'M') {
+		Pint_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Pint').classList.add("mto");
+		document.getElementById('Pint_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.Pent_e== 'X') {
+		Pent_e.innerHTML = 'OFF';
+		document.getElementById('edo_Pent').classList.add("OFF");
+		document.getElementById('Pent_est').classList.add("RED");
+	} else if (datosDOM.Pent_e== 'A') {
+		Pent_e.innerHTML = 'ON';
+		document.getElementById('edo_Pent').classList.add("ON");
+		document.getElementById('Pent_est').classList.add("GREEN");
+	} else if (datosDOM.Pent_e== 'O') {
+		Pent_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Pent').classList.add("OFF-line");
+		document.getElementById('Pent_est').classList.add("GRAY");
+	}else if (datosDOM.Pent_e== 'M') {
+		Pent_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Pent').classList.add("mto");
+		document.getElementById('Pent_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.Tint_e== 'X') {
+		Tint_e.innerHTML = 'OFF';
+		document.getElementById('edo_Tint').classList.add("OFF");
+		document.getElementById('Tint_est').classList.add("RED");
+	} else if (datosDOM.Tint_e== 'A') {
+		Tint_e.innerHTML = 'ON';
+		document.getElementById('edo_Tint').classList.add("ON");
+		document.getElementById('Tint_est').classList.add("GREEN");
+	} else if (datosDOM.Tint_e== 'O') {
+		Tint_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Tint').classList.add("OFF-line");
+		document.getElementById('Tint_est').classList.add("GRAY");
+	}else if (datosDOM.Tint_e== 'M') {
+		Tint_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Tint').classList.add("mto");
+		document.getElementById('Tint_est').classList.add("YELLOW");
+	}
+	//*************************
+	if (datosDOM.Tgas_e== 'X') {
+		Tgas_e.innerHTML = 'OFF';
+		document.getElementById('edo_Tgas').classList.add("OFF");
+		document.getElementById('Tgas_est').classList.add("RED");
+	} else if (datosDOM.Tgas_e== 'A') {
+		Tgas_e.innerHTML = 'ON';
+		document.getElementById('edo_Tgas').classList.add("ON");
+		document.getElementById('Tgas_est').classList.add("GREEN");
+	} else if (datosDOM.Tgas_e== 'O') {
+		Tgas_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Tgas').classList.add("OFF-line");
+		document.getElementById('Tgas_est').classList.add("GRAY");
+	}else if (datosDOM.Tgas_e== 'M') {
+		Tgas_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Tgas').classList.add("mto");
+		document.getElementById('Tgas_est').classList.add("YELOW");
+	}
+	if (datosDOM.Tatm_e== 'X') {
+		Tatm_e.innerHTML = 'OFF';
+		document.getElementById('edo_Tatm').classList.add("OFF");
+		document.getElementById('Tatm_est').classList.add("RED");
+	} else if (datosDOM.Tatm_e== 'A') {
+		Tatm_e.innerHTML = 'ON';
+		document.getElementById('edo_Tatm').classList.add("ON");
+		document.getElementById('Tatm_est').classList.add("GREEN");
+	} else if (datosDOM.Tatm_e== 'O') {
+		Tatm_e.innerHTML = 'OFF LINE';
+		document.getElementById('edo_Tatm').classList.add("OFF-line");
+		document.getElementById('Tatm_est').classList.add("GRAY");
+	}else if (datosDOM.Tatm_e== 'M') {
+		Tatm_e.innerHTML = 'MANTENIMIENTO';
+		document.getElementById('edo_Tatm').classList.add("mto");
+		document.getElementById('Tatm_est').classList.add("YELOW");
+	}
+}
+
+
+function actualizar_valvulashw(){
+	if (datosDOM.V1_e== 'X') {
+		document.getElementById('v1_est').classList.add("RED");
+	} else if (datosDOM.V1_e== 'A') {
+		document.getElementById('v1_est').classList.add("GREEN");
+	} else if (datosDOM.V1_e== 'O') {
+		document.getElementById('v1_est').classList.add("GRAY");
+	}else if (datosDOM.V1_e== 'M') {
+		document.getElementById('v1_est').classList.add("YELLOW");
+	}
+	//********************************
+	if (datosDOM.V2_e== 'X') {
+		document.getElementById('v2_est').classList.add("RED");
+	} else if (datosDOM.V2_e== 'A') {
+		document.getElementById('v2_est').classList.add("GREEN");
+	} else if (datosDOM.V2_e== 'O') {
+		document.getElementById('v2_est').classList.add("GRAY");
+	}else if (datosDOM.V2_e== 'M') {
+		document.getElementById('v2_est').classList.add("YELLOW");
+	}
+	//********************************
+	if (datosDOM.V3_e== 'X') {
+		document.getElementById('v3_est').classList.add("RED");
+	} else if (datosDOM.V3_e== 'A') {
+		document.getElementById('v3_est').classList.add("GREEN");
+	} else if (datosDOM.V3_e== 'O') {
+		document.getElementById('v3_est').classList.add("GRAY");
+	}else if (datosDOM.V3_e== 'M') {
+		document.getElementById('v3_est').classList.add("YELLOW");
+	}
+	//********************************
+	if (datosDOM.V4_e== 'X') {
+		document.getElementById('v4_est').classList.add("RED");
+	} else if (datosDOM.V4_e== 'A') {
+		document.getElementById('v4_est').classList.add("GREEN");
+	} else if (datosDOM.V4_e== 'O') {
+		document.getElementById('v4_est').classList.add("GRAY");
+	}else if (datosDOM.V4_e== 'M') {
+		document.getElementById('v4_est').classList.add("YELLOW");
+	}
+	//********************************
+	if (datosDOM.V5_e== 'X') {
+		document.getElementById('v5_est').classList.add("RED");
+	} else if (datosDOM.V5_e== 'A') {
+		document.getElementById('v5_est').classList.add("GREEN");
+	} else if (datosDOM.V5_e== 'O') {
+		document.getElementById('v5_est').classList.add("GRAY");
+	}else if (datosDOM.V5_e== 'M') {
+		document.getElementById('v5_est').classList.add("YELLOW");
+	}
+	//********************************
+	if (datosDOM.V6_e== 'X') {
+		document.getElementById('v6_est').classList.add("RED");
+	} else if (datosDOM.V6_e== 'A') {
+		document.getElementById('v6_est').classList.add("GREEN");
+	} else if (datosDOM.V6_e== 'O') {
+		document.getElementById('v6_est').classList.add("GRAY");
+	}else if (datosDOM.V6_e== 'M') {
+		document.getElementById('v6_est').classList.add("YELLOW");
+	}
+}
